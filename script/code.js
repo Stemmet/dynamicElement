48cca7c98a8abbc97f8728456f221b5ddb53de98
Stemmet <cameronstemmet2@gmail.com>
2022-06-10 13:24:53 +0200

    '

--- script/code.js
+++ script/code.js
@@ -1,5 +1,16 @@
-let smartPeople = ['Mikhail', 'Jesse', 'Muda', 'King', 'Cassidy']
 
-for (var key in smartPeople) {
-  document.write(smartPeople[key] + "<br>");
-}
+let newUl = document.createElement("ul");
+    let smartPeoples =
+    ['Mikhail', 'Jesse', 'Muda', 'King', 'Cassidy'];
+    function smartPeople(){
+    let ul = document.body.appendChild(newUl);
+    smartPeoples.forEach( (item )=>{
+        ul.innerHTML +=
+        `<li>${item}</li>`;
+        });
+    }
+    smartPeople();
+
+// for (var key in smartPeople) {
+//   document.write(smartPeople[key] + "<br>");
+// }
